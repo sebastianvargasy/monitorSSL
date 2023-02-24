5d9630db55d67cfd0f1835c2374d0af6c24047e7
sebastianvargasy <64619819+sebastianvargasy@users.noreply.github.com>
2023-02-24 11:47:06 -0300

    Update index.py

--- index.py
+++ index.py
@@ -1,95 +1,38 @@
-# -*- encoding: utf-8 -*-
-# requires a recent enough python with idna support in socket
-# pyopenssl, cryptography and idna
+import ssl
+import socket
+from datetime import datetime
+import streamlit as st
 
-from OpenSSL import SSL
-from cryptography import x509
-from cryptography.x509.oid import NameOID
-import idna
-
-from socket import socket
-from collections import namedtuple
-
-HostInfo = namedtuple(field_names='cert hostname peername', typename='HostInfo')
-
-HOSTS = [
-    ('whilolab.cl', 443),
-]
-
-def verify_cert(cert, hostname):
-    # verify notAfter/notBefore, CA trusted, servername/sni/hostname
-    cert.has_expired()
-    # service_identity.pyopenssl.verify_hostname(client_ssl, hostname)
-    # issuer
-
-def get_certificate(hostname, port):
-    hostname_idna = idna.encode(hostname)
-    sock = socket()
-
-    sock.connect((hostname, port))
-    peername = sock.getpeername()
-    ctx = SSL.Context(SSL.SSLv23_METHOD) # most compatible
-    ctx.check_hostname = False
-    ctx.verify_mode = SSL.VERIFY_NONE
-
-    sock_ssl = SSL.Connection(ctx, sock)
-    sock_ssl.set_connect_state()
-    sock_ssl.set_tlsext_host_name(hostname_idna)
-    sock_ssl.do_handshake()
-    cert = sock_ssl.get_peer_certificate()
-    crypto_cert = cert.to_cryptography()
-    sock_ssl.close()
-    sock.close()
-
-    return HostInfo(cert=crypto_cert, peername=peername, hostname=hostname)
-
-def get_alt_names(cert):
+# Función para verificar el certificado SSL de un sitio web
+def verificar_certificado(url):
     try:
-        ext = cert.extensions.get_extension_for_class(x509.SubjectAlternativeName)
-        return ext.value.get_values_for_type(x509.DNSName)
-    except x509.ExtensionNotFound:
-        return None
+        cert = ssl.get_server_certificate((url, 443))
+        x509 = ssl.PEM_cert_to_X509(cert)
+        expira = x509.get_notAfter().decode("ascii")
+        expira = datetime.strptime(expira, '%Y%m%d%H%M%SZ')
+        dias_restantes = (expira - datetime.now()).days
+        return {
+            'url': url,
+            'expira_en': dias_restantes
+        }
+    except (ssl.CertificateError, ssl.SSLError, socket.gaierror):
+        return {
+            'url': url,
+            'expira_en': 'No se pudo verificar'
+        }
 
-def get_common_name(cert):
-    try:
-        names = cert.subject.get_attributes_for_oid(NameOID.COMMON_NAME)
-        return names[0].value
-    except x509.ExtensionNotFound:
-        return None
+# Configurar la aplicación Streamlit
+st.set_page_config(page_title='SSL Checker')
 
-def get_issuer(cert):
-    try:
-        names = cert.issuer.get_attributes_for_oid(NameOID.COMMON_NAME)
-        return names[0].value
-    except x509.ExtensionNotFound:
-        return None
+# Mostrar un formulario para ingresar la URL
+st.title('SSL Checker')
+url = st.text_input('Ingrese la URL del sitio web a verificar')
 
+# Verificar el certificado SSL y mostrar los resultados en una tabla
+if url:
+    st.write('Verificando el certificado SSL para:', url)
+    resultado = verificar_certificado(url)
+    st.write('El certificado SSL para', url, 'expira en', resultado['expira_en'], 'días')
+    st.write('Resultados:')
+    st.table([resultado])
 
-def print_basic_info(hostinfo):
-    s = '''» {hostname} « … {peername}
-    \tcommonName: {commonname}
-    \tSAN: {SAN}
-    \tissuer: {issuer}
-    \tnotBefore: {notbefore}
-    \tnotAfter:  {notafter}
-    '''.format(
-            hostname=hostinfo.hostname,
-            peername=hostinfo.peername,
-            commonname=get_common_name(hostinfo.cert),
-            SAN=get_alt_names(hostinfo.cert),
-            issuer=get_issuer(hostinfo.cert),
-            notbefore=hostinfo.cert.not_valid_before,
-            notafter=hostinfo.cert.not_valid_after
-    )
-    print(s)
-
-def check_it_out(hostname, port):
-    hostinfo = get_certificate(hostname, port)
-    print_basic_info(hostinfo)
-
-
-import concurrent.futures
-if __name__ == '__main__':
-    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as e:
-        for hostinfo in e.map(lambda x: get_certificate(x[0], x[1]), HOSTS):
-            print_basic_info(hostinfo)
